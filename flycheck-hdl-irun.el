2ab41a9f7af44795facbcb232ac8a09071af37e2
Colin Marquardt <github@marquardt-home.de>
2016-01-22 15:34:59 +0100

    Use flycheck-def-config-file-var; extend docs.

--- flycheck-hdl-irun.el
+++ flycheck-hdl-irun.el
@@ -4,18 +4,21 @@
 ;;
 ;; Author: Colin Marquardt <colin@marquardt-home.de>
 ;; Created: 21 January 2016
-;; Version: 0.1
+;; Version: 0.2
 ;; Package-Requires: ((flycheck "0.25"))
 
 ;;; Commentary:
 
-;; This package adds support for Cadence irun to flycheck. To use it, add
-;; to your init.el:
+;; This package adds support for Cadence irun
+;; (http://www.cadence.com/products/fv/enterprise_simulator/pages/default.aspx)
+;; to flycheck.  To use it, add something like the following your init.el:
 
 ;; (require 'flycheck-hdl-irun)
 ;; (add-hook 'vhdl-mode-hook 'flycheck-mode)
 ;; (add-hook 'verilog-mode-hook 'flycheck-mode)
 ;; (add-hook 'systemc-mode-hook 'flycheck-mode)
+;; (setq flycheck-hdl-irun-hdlvar
+;;   (concat (getenv "PRJ_HOME") "/env/df2/hdl.var"))
 
 ;;; License:
 
@@ -40,12 +43,18 @@
 ;;; Code:
 (require 'flycheck)
 
-(setq flycheck-irun-hdlvar "hdl.var")
+(flycheck-def-config-file-var flycheck-hdl-irun-hdlvar
+    hdl-irun "hdl.var")
+(flycheck-def-config-file-var flycheck-hdl-irun-cdslib
+    hdl-irun nil)
 
 (flycheck-define-checker hdl-irun
-  "A HDL checker using Cadence irun."
+  "A checker for hardware description languages (HDLs) using Cadence irun.
+irun is part of the Cadence Incisive Enterprise Simulator
+(http://www.cadence.com/products/fv/enterprise_simulator/pages/default.aspx)."
   :command ("irun" "-compile"
-            (config-file "-hdlvar" flycheck-irun-hdlvar)
+            (config-file "-hdlvar" flycheck-hdl-irun-hdlvar)
+            (config-file "-cdslib" flycheck-hdl-irun-cdslib)
             source)
   :error-patterns
   ((info line-start (one-or-more (in "a-z"))
@@ -55,7 +64,7 @@
             ": *W," (id (one-or-more (in "A-Z0-9"))) " (" (file-name) "," line "|" column "): "
             (message) line-end)
    (error line-start (one-or-more (in "a-z"))
-          ": *E," (id (one-or-more (in "A-Z0-9"))) " (" (file-name) "," line "|" column "): "
+          (or ": *E," ": *F,") (id (one-or-more (in "A-Z0-9"))) " (" (file-name) "," line "|" column "): "
           (message) line-end))
   :modes (systemc-mode vhdl-mode verilog-mode))
 
